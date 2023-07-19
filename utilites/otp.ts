023e85762a4af0d2b9a089de7ff09fbdb28fdad0
ndn23 <100492179+ndn23@users.noreply.github.com>
2023-07-19 14:34:09 +0300

    o

--- utilites/otp.ts
+++ utilites/otp.ts
@@ -1,17 +1,17 @@
 const client = require("twilio")(
-  "AC796018843aab82291cfdd2484cee2887",
-  "9102a23e0cff05b5e4abe620e173b688"
+  process.env.Account_SID,
+  process.env.Auth_Token
 );
 
 const OtpSend = async (phonenumber: string) => {
   await client.verify.v2
-    .services("VA469ab848f8466171525f40765920b2f3")
+    .services(process.env.Service_SID)
     .verifications.create({ to: phonenumber, channel: "sms" });
 };
 
 const OtpVerfiy = async (phonenumber: string, code: string) => {
   return await client.verify.v2
-    .services("VA469ab848f8466171525f40765920b2f3")
+    .services(process.env.Service_SID)
     .verificationChecks.create({ to: phonenumber, code });
 };
 
